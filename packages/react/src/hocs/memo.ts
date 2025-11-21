e98475e7da312c85585eb28bcd088d4df5e2bbc3
kimzeze <fridaynight@kakao.com>
2025-11-21 09:39:25 +0900

    feat(hocs): memo HOC 구현

--- packages/react/src/hocs/memo.ts
+++ packages/react/src/hocs/memo.ts
@@ -12,10 +12,16 @@ import { shallowEquals } from "../utils";
  */
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
   const MemoizedComponent: FunctionComponent<P> = (props) => {
-    // 여기를 구현하세요.
-    // useRef를 사용하여 이전 props와 렌더링 결과를 저장해야 합니다.
-    // equals 함수로 이전 props와 현재 props를 비교하여 렌더링 여부를 결정합니다.
-    return Component(props);
+    const cache = useRef<{ props: P; result: VNode } | null>(null);
+
+    if (!cache.current || !equals(cache.current.props, props)) {
+      cache.current = {
+        props,
+        result: Component(props),
+      };
+    }
+
+    return cache.current.result;
   };
 
   MemoizedComponent.displayName = `Memo(${Component.displayName || Component.name})`;
