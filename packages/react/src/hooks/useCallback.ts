1e9dddf03719142fd944c976c99a67abb60f71d4
kimzeze <fridaynight@kakao.com>
2025-11-21 09:35:18 +0900

    feat(hooks): useCallback 구현

--- packages/react/src/hooks/useCallback.ts
+++ packages/react/src/hooks/useCallback.ts
@@ -9,8 +9,7 @@ import { useMemo } from "./useMemo";
  * @param deps - 의존성 배열
  * @returns 메모이제이션된 콜백 함수
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useCallback = <T extends (...args: any[]) => any>(callback: T, deps: DependencyList): T => {
-  // 여기를 구현하세요.
-  // useMemo를 사용하여 구현할 수 있습니다.
-  return callback;
+  return useMemo(() => callback, deps);
 };
